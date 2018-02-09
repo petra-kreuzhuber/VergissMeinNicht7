1738f1fb612ecad48351a32c10a0ea2fa70ea48d
nicolepayr <ni.payr@gmail.com>
2018-02-09 16:49:51 +0100

    start menu
    Start menu

---
+++ Prototype1/Assets/ButtonMng.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class ButtonMng : MonoBehaviour
+{
+
+	public void newGame(string newGameLevel)
+    {
+        SceneManager.LoadScene(newGameLevel);
+    }
+}
