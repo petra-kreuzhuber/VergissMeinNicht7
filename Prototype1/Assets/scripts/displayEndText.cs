1b37dbdc9dfe9c9cd15a42282d502f9ff2a817ba
vniedermoser <32902792+vniedermoser@users.noreply.github.com>
2018-02-20 23:51:42 +0100

    Texte

--- Prototype1/Assets/scripts/displayEndText.cs
+++ Prototype1/Assets/scripts/displayEndText.cs
@@ -35,7 +35,7 @@ public class displayEndText : MonoBehaviour {
 	}
 	*/
 	IEnumerator WaitForSec(){
-		yield return new WaitForSeconds (360);
+		yield return new WaitForSeconds (300);
 		display = true;
 		//Destroy (gameObject);
 	}
@@ -44,7 +44,7 @@ public class displayEndText : MonoBehaviour {
 	void OnGUI()
 	{
 		if (display == true) {
-			GUI.Box (new Rect (0, 50, Screen.width, 24), text);
+			GUI.Box (new Rect (0, 100, Screen.width, 150), "In Österreich gibt es ca. 130000 Menschen, die an einer dementiellen Erkrankung leiden \n Aufgrund des kontinuierlichen Altersanstiegs in der Bevölkerung wird sich diese Anzahl bis zum Jahr 2050 verdoppeln \n und der Betreuungs- und Pflegebedarf wird somit weiter steigen. \n Weit mehr als die hHölfte aller Demenzpatienten haben die Alzheimerkrankheit. \n AlzheimerpatientInnen leiden an langsam fortschreitenden Gedächtnis- und Denkstörungen und infolge dessen an \n Beeinträchtigungen der Alltagsaktivität. Ebenfalls gehören das Vergessen von kurz zurückliegenden Ereignissen, Problemen mit der zeitlichen und örtlichen \n Orientierung sowie Schwierigkeiten beim Fällen von Entscheidungen \n zu den Warnzeichen für beginnende Demenz. ");
 		}
 	}
 }
