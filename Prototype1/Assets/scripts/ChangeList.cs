24a82b3268df2275604a3ac6babb1b5e7ecefe99
nicolepayr <ni.payr@gmail.com>
2018-02-22 11:51:10 +0100

    Revert "Merge branch 'master' of https://github.com/petra-kreuzhuber/VergissMeinNicht7"
    This reverts commit 5f1eec0df8c9a041c1ee0d39855be0ddf4407f71, reversing
    changes made to 72e2e21b655ec633415bd005f40aaf2111c9bb66.

--- Prototype1/Assets/scripts/ChangeList.cs
+++ Prototype1/Assets/scripts/ChangeList.cs
@@ -16,15 +16,6 @@ public class ChangeList : MonoBehaviour {
     public AudioSource[] sounds;
     public AudioSource audio1;
     public AudioSource audio2;
-    public double timer;
-
-	//Variablen für Audio in der Update
-    bool biggerThanTimer = false;
-	bool biggerThanTimer2 = false;
-	bool biggerThanTimer3 = false;
-	bool biggerThanTimer4 = false;
-	bool biggerThanTimer5 = false;
-	bool biggerThanTimer6 = false;
 
 
     bool PlaySound;
@@ -42,58 +33,33 @@ public class ChangeList : MonoBehaviour {
         sounds = GetComponents<AudioSource>();
         audio1 = sounds[1];
         audio2 = sounds[2];
-        timer = Time.time;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		//erster Katzenruf gleich am Anfang
-        if(Time.time > timer + 20 && !biggerThanTimer)
+		if(cat && pills && phone && pencil)
         {
-            biggerThanTimer = true;
+            GetComponent<MeshRenderer>().material = Resources.Load("Liste4") as Material;
+        } else if(cat && pills && pencil)
+        {
+         //   StartCoroutine(Wait());
+            audio2.Play();
+            GetComponent<MeshRenderer>().material = Resources.Load("Liste3") as Material;
+        } else if(cat && pencil)
+        {
             audio1.Play();
-        }
-		//Timer wird nach Stiftaufnahme neu gesetzt
-		if (pencil && !biggerThanTimer2) {
-			biggerThanTimer2 = true;
-			biggerThanTimer3 = true;
-			timer = Time.time;
-		}
-
-		// 2. Katzenruf nach Aufnahme von Stift und bestimmter Zeit und Katzenaufgabe gilt als abgeschlossen
-		if (Time.time > timer + 20 && biggerThanTimer3) {
-			biggerThanTimer3 = false;
-			audio1.Play ();
-			cat = true;
-		}
-
-		// Timer wird nach Pillenaufnahme neu gesetzt
-		if (pills && !biggerThanTimer4) {
-			biggerThanTimer4 = true;
-			biggerThanTimer5 = true;
-			timer = Time.time;
-		}
-
-		//Nach Pillenaufnahme und bestimmter Zeit wird Telefongeräusch abgespielt
-		if (Time.time > timer + 20 && biggerThanTimer5) {
-			biggerThanTimer5 = false;
-			biggerThanTimer6 = true;
-			audio2.Play ();
-			timer = Time.time;
-		}
-
-		//nach bestimmter Zeit wird Telephon abgeschlossen 
-		if (Time.time > timer + 25 && biggerThanTimer6) {
-			phone = true;
-			//ende?!
-		}
-
-		//updatet die Liste je nach Variablenzustand
-		listUpdate ();
+            GetComponent<MeshRenderer>().material = Resources.Load("Liste2") as Material;
+        } 
     }
 
+    /*
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(5.0f);
+        PlaySound = true;
+    }
+    */
 
-	//Getter und Setter
     bool getCat()
     {
         return cat;
@@ -117,21 +83,4 @@ public class ChangeList : MonoBehaviour {
     void setPencil(bool NEW) {
         pencil = NEW;
         }
-
-
-	//updatet Liste je nach Variablenzustand
-	void listUpdate() {
-		//Wenn alle Aufgaben erfüllt sind wird die vollstänig abgehakte Liste angezeigt
-		if (cat && pills && phone && pencil) {
-			GetComponent<MeshRenderer> ().material = Resources.Load ("Liste4") as Material;
-		}
-		//Wenn Pillen und Katze schon erfüllt werden diese beiden abgehackt
-		else if (cat && pills && pencil) { 
-			GetComponent<MeshRenderer> ().material = Resources.Load ("Liste3") as Material;
-		}
-		//wenn Katze schon erfüllt wird dieses abgehackt
-		else if (cat && pencil) {
-			GetComponent<MeshRenderer> ().material = Resources.Load ("Liste2") as Material;
-		}
-	}
 }
